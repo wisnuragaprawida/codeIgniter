4c94733c8fe1cee7641d761aa8d7145fe573953f
wisnuragaprawida <wisnuraga418@gmail.com>
2020-03-20 13:59:52 +0700

    menyelesaikan template login dan membuat database

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -3,9 +3,24 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-
-    public function index()
+    public function __construct()
     {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+    public function login()
+    {
+        $data['title'] = 'Login';
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+    public function registration()
+    {
+        $data['title'] = 'register';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/registration');
+        $this->load->view('templates/auth_footer');
     }
 }
