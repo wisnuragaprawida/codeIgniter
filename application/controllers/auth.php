171fc223c8dc84de77f891e96b10e68982b85541
wisnuragaprawida <wisnuraga418@gmail.com>
2020-03-23 15:03:14 +0700

    menyelesaikan halaman registrasi

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -19,14 +19,35 @@ class Auth extends CI_Controller
     public function registration()
     {
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'this email alredy regitered!!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[6]|matches[password2]', [
+            'matches' => 'password does not match!',
+            'min_length' => 'password too short!'
+
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
         if ($this->form_validation->run() == false) {
             $data['title'] = 'register';
             $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/registration');
             $this->load->view('templates/auth_footer');
         } else {
-            echo "data berhasil";
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_create' => time()
+            ];
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+           create acount succsess!!, please login.</div>');
+            redirect('auth/login');
         }
     }
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -16,6 +16,7 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">LOGIN</h1>
                                     </div>
+                                    <?= $this->session->flashdata('message'); ?>
                                     <form class="user">
                                         <div class="form-group">
                                             <input type="text" class="form-control form-control-user" id="exampleInputEmail" name="email" aria-describedby="emailHelp" placeholder="Enter Email Address...">
